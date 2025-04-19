3e573e729f0aef8ca2539c13544534f949f3c04d
shalviaa <shalviars@gmail.com>
2025-04-19 16:09:22 +0700

    tambahakn kategori

--- PasarLocal/database/migrations/2025_04_08_062309_create_kategori_produk_table.php
+++ PasarLocal/database/migrations/2025_04_08_062309_create_kategori_produk_table.php
@@ -18,20 +18,19 @@ return new class extends Migration
 
         DB::afterCommit(function () {
             DB::table('kategori_produk')->insert([
-                ['nama_kategori' => 'Sayur', 'gambar' => 'sayur.jpg'],
-                ['nama_kategori' => 'Buah-buahan', 'gambar' => 'buah.jpg'],
-                ['nama_kategori' => 'Daging Sapi', 'gambar' => 'daging_sapi.jpg'],
-                ['nama_kategori' => 'Daging Ayam', 'gambar' => 'daging_ayam.jpg'],
-                ['nama_kategori' => 'Ikan Laut', 'gambar' => 'ikan_laut.jpg'],
-                ['nama_kategori' => 'Rempah & Bumbu', 'gambar' => 'bumbu.jpg'],
-                ['nama_kategori' => 'Makanan Instan', 'gambar' => 'makanan_instan.jpg'],
-                ['nama_kategori' => 'Produk Olahan Daging', 'gambar' => 'produk_olahan_daging.jpg'],
-                ['nama_kategori' => 'Produk Olahan Nabati', 'gambar' => 'produk_olahan_nabati.jpg'],
-                ['nama_kategori' => 'Bahan Pokok', 'gambar' => 'bahan_pokok.jpg'],
-                ['nama_kategori' => 'Minuman', 'gambar' => 'minuman.jpg'],
+                ['nama_kategori' => 'Sayur', 'gambar' => 'sayur.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Buah-buahan', 'gambar' => 'buah.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Daging Sapi', 'gambar' => 'daging_sapi.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Daging Ayam', 'gambar' => 'daging_ayam.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Ikan Laut', 'gambar' => 'ikan_laut.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Rempah & Bumbu', 'gambar' => 'bumbu.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Makanan Instan', 'gambar' => 'makanan_instan.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Produk Olahan Daging', 'gambar' => 'produk_olahan_daging.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Produk Olahan Nabati', 'gambar' => 'produk_olahan_nabati.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Bahan Pokok', 'gambar' => 'bahan_pokok.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
+                ['nama_kategori' => 'Minuman', 'gambar' => 'minuman.jpg', 'created_at' => '2025-04-02', 'updated_at' => null],
             ]);
-            
-        });
+        });        
     }
 
     public function down(): void
